25bef5feb0d7d6c0e5bfa89e19241df7f2f6ef22
VanillaSieghart <kirasayura@gmail.com>
2025-04-03 11:31:24 -0300

    Espada, Machado ou Lança! Fire Emblem Jokenpo
    nada muito diferente, meus primeiros projetos, mas nesse o botão funciona

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-//criar uma lista com cada time
-standUsers = ["", "", ""]
-viloes = ["","", ""]
-
-forcaStandUsers = 0
-forcaViloes = 0 
-alert ("O Array agr tem:" + standUsers)
-for(let i=0; i<3; i++){
-  escolhaStand = prompt("Digite o nome do seu Stand: " + (i + 1))
-  standUsers[i] = escolhaStand
-  //calcular a força dos usarios de stand
-  //forcaStandUsers = forcaStandUsersm + Math.floor(Math.random() * 10) + 1
-  forcaStandUsers += Math.floor(Math.random() * 10) + 1
+function jogar(){levels = prompt ("Quanto de level você tem?")
+if (levels < 18) {
+  alert("Você ainda não está pronto!")
 }
-console.log("agr o array Standuser contem: " + standUsers)
-console.log("Viloes: " + viloes)
-for(let i=0; i<3; i++){
-  indiceAleatorio = Math.floor(Math.random() * 5)
-  viloesPossiveis = ["Yoshikage Kira", "Dio Brando", "Enrico Pucci", "Kars", "Diavolo"]
-  viloes[i] = viloesPossiveis[indiceAleatorio]
-  //calcular a forca dos viloes
-  forcaViloes += Math.floor(Math.random() * 10) + 1
-}
-console.log("final viloes " + viloes)
-
-//comparar os dois times pra ver quem que que venceu.
-if(forcaStandUsers > forcaViloes){
-  alert("Stands mais fortes que os vilões! Você ganhou! Sua força foi: " + forcaStandUsers)
-}
-else{
-  if (forcaStandUsers < forcaViloes){
-    alert("Vilões sempre vencem! Você perdeu! a força deles foi: " + forcaViloes)
+if (levels >= 18) {
+  escolhaJogador = prompt ("Escolha 1espada, 2machado ou 3lança?")
+  escolhaComputador = Math.floor (Math.random() * 3) + 1
+  
+  //Jogador 1espada, computador 1espada -> empate
+  //Jogador 2machado, computador 2machado -> empate
+  //Jogador 3lança, computador 3 lança -> empate
+  if (escolhaJogador == escolhaComputador) {
+    alert("Empate de Guerreiros!")
   }
- else{
-   alert("Empate! Pronto para uma nova batalha?")
- }
-}
+  
+  //Jogador 1espada, Computador 2machado -> jogador vence
+  if (escolhaJogador == 1) {
+    if (escolhaComputador == 2) {
+      alert("Guerreiro venceu, Sir Pc escolheu o machado!")
+    }
+    //jogador 1espada, computador 3lança -> computador vence
+    if (escolhaComputador == 3) {
+      alert("Sir Pc venceu, escolheu a lança!")
+    }
+  }
+  //Jogador 2machado, computador 3lança -> jogador ganha
+  if (escolhaJogador == 2) {
+    if (escolhaComputador == 3) {
+      alert("Guerreiro venceu, Sir Pc escolheu a lança")
+    }
+    if (escolhaComputador == 1) {
+      alert("Sir Pc venceu, escolheu a espada!")
+    }
+  }
+  //Jogador 3lança, computador 1espada -> jogador vence
+  if (escolhaJogador == 3) {
+    if (escolhaComputador == 1) {
+      alert("Guerreiro vence, Sir Pc escolheu a espada")
+    }
+    if (escolhaComputador == 2) {
+      alert("Sir Pc venceu, escolheu o machado")
+    }
+  }
+ 
+
+  alert("Escolha do Sir Pc foi " + escolhaComputador)
+  }
+} 
